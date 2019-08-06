3f9792c233123d13c8520f33c0ab41028f54b272
Werner Kotze <werner.kotze@clevva.com>
2019-08-06 20:04:40 +0200

    initial commit

---
+++ server/routes /api/posts.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts 
+router.get('/', (req, res) => {
+    res.send('hello');
+});
+
+// Add Post 
+ 
+// Delete Post 
+
+// Update Post 
+// 
+module.exports = router;
