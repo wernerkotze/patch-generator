f10064477fd4e3a41ac66470d9315b1d7ae73717
Werner Kotze <werner.kotze@clevva.com>
2019-09-08 12:55:53 +0200

    master: completed backend base

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,13 +4,35 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // get posts 
-router.get('/', (req, res) => {
-    res.send('hello');
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
 });
 
 // add posts
-
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send()
+})
 
 // delete posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send()
+})
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://werner:S9q2moGW051Zis9O@vue-express-3wz6l.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    })
+
+    return client.db('vue-express').collection('posts')
+}
 
 module.exports = router;
